6a0a34da03ef5c2dc59df58e0d2df98b459be02d
Krittin Chaowakarn <krittin.chao@gmail.com>
2024-06-12 18:13:33 +0700

    Update

--- image-processing/excercises/ex5-difference-keying.py
+++ image-processing/excercises/ex5-difference-keying.py
@@ -3,7 +3,6 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 import scipy
-import statistics
 
 # Paths
 input_path = 'sample-images/'
@@ -17,17 +16,8 @@ l = 1024
 
 img = cv.resize(img, (l, l))
 img_bg = cv.resize(img_bg, (l, l))
-# img_next = cv.resize(img_next, (l, l))
 
-
-def adjust_foreground_mask(foreground_mask):
-    high_mask = np.any(foreground_mask > 200, axis=-1)
-    
-    foreground_mask[high_mask] = 255
-    
-    return foreground_mask
-
-
+# Define the corner filter
 corner_filter = np.array([[-1, -2, 1], 
                           [-2, 4, -2], 
                           [-1, -2, 1]]) / 4
@@ -37,57 +27,79 @@ def apply_filter_to_color_image(image, filter_kernel):
     filtered_channels = [scipy.signal.convolve2d(channel, filter_kernel, mode='same') for channel in channels]
     return cv.merge(filtered_channels)
 
-# img = img[:300, :300]
-# img_bg = img_bg[:300, :300]
-
 def apply_variance_check(img, img_bg, v):
     new_img = np.zeros(img.shape)
     for i in range(img.shape[0]):
         for j in range(img.shape[1]):
             var = ((img[i,j] - img_bg[i,j])^2)/255
-            # print(var)
             if np.all(var < v):
                 new_img[i,j] = img[i,j]
-            else :
+            else:
                 new_img[i,j] = 0
     return new_img.astype(np.uint8)
 
+img_fg = apply_variance_check(img, img_bg, 0.925)
 
-img_fg = apply_variance_check(img, img_bg, 0.8)
+def apply_median_blur(img_fg, kernel):
+    b, g, r = cv.split(img_fg)
+    b = cv.medianBlur(b, kernel)
+    g = cv.medianBlur(g, kernel)
+    r = cv.medianBlur(r, kernel)
+    return cv.merge([b, g, r])
+
+
 bilateral_filtered = cv.bilateralFilter(img_fg, 9, 75, 75)
 
-
-b, g, r = cv.split(img_fg)
-b = cv.medianBlur(b, 5)
-g = cv.medianBlur(g, 5)
-r = cv.medianBlur(r, 5)
-median_filtered = cv.merge([b, g, r])
+median_filtered = apply_median_blur(img_fg, 17)
+median_filtered = apply_median_blur(median_filtered, 5)
 
 nlm_denoised = cv.fastNlMeansDenoisingColored(img_fg, None, 10, 10, 7, 21)
 
-plt.subplot(1,3,1)
+
+num_labels, labels, stats, centroids = cv.connectedComponentsWithStats(cv.cvtColor(median_filtered, cv.COLOR_BGRA2GRAY), connectivity=8)
+
+print(num_labels, labels, stats, centroids)
+
+for i in range(1, num_labels):  # Skip background label 0
+    centroid_x, centroid_y = centroids[i]
+    print(f"Centroid of object {i}: ({centroid_x}, {centroid_y})")
+
+# Visualize the centroids on the image
+plt.imshow(cv.cvtColor(median_filtered, cv.COLOR_GRAY2RGB))
+for i in range(1, num_labels):  # Skip background label 0
+    centroid_x, centroid_y = centroids[i]
+    plt.scatter(centroid_x, centroid_y, color='red', s=30)
+plt.title('Filtered Image with Centroids')
+plt.show()
+'''
+# Set figure size
+plt.figure(figsize=(15, 10))
+
+plt.subplot(2, 3, 1)
 plt.imshow(cv.cvtColor(img, cv.COLOR_BGR2RGB))
 plt.title('Original Image')
 
-plt.subplot(1,3,2)
+plt.subplot(2, 3, 2)
 plt.imshow(cv.cvtColor(img_bg, cv.COLOR_BGR2RGB))
-plt.title('Backround Image')
+plt.title('Background Image')
 
-plt.subplot(1,3,3)
+plt.subplot(2, 3, 3)
 plt.imshow(cv.cvtColor(img_fg, cv.COLOR_BGR2RGB))
 plt.title('Background Removed Image')
 
-plt.subplot(2,3,4)
+plt.subplot(2, 3, 4)
 plt.imshow(cv.cvtColor(bilateral_filtered, cv.COLOR_BGR2RGB))
-plt.title('Background Removed Image')
+plt.title('Bilateral Filtered Image')
 
-plt.subplot(2,3,5)
+plt.subplot(2, 3, 5)
 plt.imshow(cv.cvtColor(median_filtered, cv.COLOR_BGR2RGB))
-plt.title('Background Removed Image')
+plt.title('Median Filtered Image')
 
-plt.subplot(2,3,6)
+plt.subplot(2, 3, 6)
 plt.imshow(cv.cvtColor(nlm_denoised, cv.COLOR_BGR2RGB))
-plt.title('Background Removed Image')
+plt.title('NLM Denoised Image')
 
-
+plt.tight_layout()
 plt.show()
+
+'''
